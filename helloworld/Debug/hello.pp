6ac852850996b077173a867a9d12ca9a712e75a0
unknown <cwgeary@BLAKE.ecenet.wpi.edu>
2014-11-04 21:18:18 -0500

    Added simulator project files

---
+++ helloworld/Debug/hello.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+hello.obj: ../hello.c
+hello.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdio.h
+hello.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/linkage.h
+hello.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdarg.h
+
+../hello.c: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdarg.h: 
---
+++ stereoloop_squareleft/Debug/stereoloop.pp
@@ -0,0 +1,49 @@
+# FIXED
+
+stereoloop.obj: ../stereoloop.c
+stereoloop.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdio.h
+stereoloop.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/linkage.h
+stereoloop.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdarg.h
+stereoloop.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/c6x.h
+stereoloop.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/vect.h
+stereoloop.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdint.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_chip.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_stdinc.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_stdinchal.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/tistdtypes.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_chiphal.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_emifhal.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_chip.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_irq.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_irqhal.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_timer.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_timerhal.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_mcbsp.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_mcbsphal.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/DSK6713/c6000/dsk6713/include/dsk6713.h
+stereoloop.obj: C:/Program\ Files/Texas\ Instruments/DSK6713/c6000/dsk6713/include/dsk6713_aic23.h
+
+../stereoloop.c: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/c6x.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/vect.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdint.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_chip.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_stdinc.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_stdinchal.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/tistdtypes.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_chiphal.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_emifhal.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_chip.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_irq.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_irqhal.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_timer.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_timerhal.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_mcbsp.h: 
+C:/Program\ Files/Texas\ Instruments/C6xCSL/include/csl_mcbsphal.h: 
+C:/Program\ Files/Texas\ Instruments/DSK6713/c6000/dsk6713/include/dsk6713.h: 
+C:/Program\ Files/Texas\ Instruments/DSK6713/c6000/dsk6713/include/dsk6713_aic23.h: 
