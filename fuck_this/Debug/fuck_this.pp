47b128e0e4101ae37f3114039f03a39707f6a40b
unknown <zpjasensky@HADAMARD.ecenet.wpi.edu>
2014-12-10 23:44:25 -0500

    added new project 'fuck_this' with working stereoloop with frame processing

---
+++ fuck_this/Debug/fuck_this.pp
@@ -0,0 +1,67 @@
+# FIXED
+
+fuck_this.obj: ../fuck_this.c
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdio.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/linkage.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdarg.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/c6x.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/vect.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdint.h
+fuck_this.obj: C:/ti/include/csl.h
+fuck_this.obj: C:/ti/include/csl_chip.h
+fuck_this.obj: C:/ti/include/csl_stdinc.h
+fuck_this.obj: C:/ti/include/csl_stdinchal.h
+fuck_this.obj: C:/ti/include/tistdtypes.h
+fuck_this.obj: C:/ti/include/csl_chiphal.h
+fuck_this.obj: C:/ti/include/csl_emifhal.h
+fuck_this.obj: C:/ti/include/csl_chip.h
+fuck_this.obj: C:/ti/include/csl_irq.h
+fuck_this.obj: C:/ti/include/csl_irqhal.h
+fuck_this.obj: C:/ti/include/csl_timer.h
+fuck_this.obj: C:/ti/include/csl_timerhal.h
+fuck_this.obj: C:/ti/include/csl_mcbsp.h
+fuck_this.obj: C:/ti/include/csl_mcbsphal.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/math.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/float.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/access.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/elfnames.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/mathf.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/unaccess.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/mathl.h
+fuck_this.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/unaccess.h
+fuck_this.obj: C:/ti/DSK6713/c6000/dsk6713/include/dsk6713.h
+fuck_this.obj: C:/ti/DSK6713/c6000/dsk6713/include/dsk6713_aic23.h
+fuck_this.obj: ../lowpass_50.h
+
+../fuck_this.c: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/c6x.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/vect.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/stdint.h: 
+C:/ti/include/csl.h: 
+C:/ti/include/csl_chip.h: 
+C:/ti/include/csl_stdinc.h: 
+C:/ti/include/csl_stdinchal.h: 
+C:/ti/include/tistdtypes.h: 
+C:/ti/include/csl_chiphal.h: 
+C:/ti/include/csl_emifhal.h: 
+C:/ti/include/csl_chip.h: 
+C:/ti/include/csl_irq.h: 
+C:/ti/include/csl_irqhal.h: 
+C:/ti/include/csl_timer.h: 
+C:/ti/include/csl_timerhal.h: 
+C:/ti/include/csl_mcbsp.h: 
+C:/ti/include/csl_mcbsphal.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/math.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/float.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/access.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/elfnames.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/mathf.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/unaccess.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/mathl.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.2/include/unaccess.h: 
+C:/ti/DSK6713/c6000/dsk6713/include/dsk6713.h: 
+C:/ti/DSK6713/c6000/dsk6713/include/dsk6713_aic23.h: 
+../lowpass_50.h: 
---
+++ fuck_this/Debug/subdir_vars.mk
@@ -0,0 +1,67 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../C6713.cmd 
+
+SA_SRCS += \
+../bitrev.sa \
+../cfftr2_dit.sa 
+
+ASM_SRCS += \
+../vectors.asm 
+
+C_SRCS += \
+../digitrev_index.c \
+../fuck_this.c 
+
+OBJS += \
+./bitrev.obj \
+./cfftr2_dit.obj \
+./digitrev_index.obj \
+./fuck_this.obj \
+./vectors.obj 
+
+ASM_DEPS += \
+./vectors.pp 
+
+C_DEPS += \
+./digitrev_index.pp \
+./fuck_this.pp 
+
+SA_DEPS += \
+./bitrev.pp \
+./cfftr2_dit.pp 
+
+C_DEPS__QUOTED += \
+"digitrev_index.pp" \
+"fuck_this.pp" 
+
+OBJS__QUOTED += \
+"bitrev.obj" \
+"cfftr2_dit.obj" \
+"digitrev_index.obj" \
+"fuck_this.obj" \
+"vectors.obj" 
+
+SA_DEPS__QUOTED += \
+"bitrev.pp" \
+"cfftr2_dit.pp" 
+
+ASM_DEPS__QUOTED += \
+"vectors.pp" 
+
+SA_SRCS__QUOTED += \
+"../bitrev.sa" \
+"../cfftr2_dit.sa" 
+
+C_SRCS__QUOTED += \
+"../digitrev_index.c" \
+"../fuck_this.c" 
+
+ASM_SRCS__QUOTED += \
+"../vectors.asm" 
+
+
